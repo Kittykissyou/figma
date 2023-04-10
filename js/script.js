d7537f191403df50a8ddb556a956458d1a555ccc
kittyKissYou <ubiuka@yandex.ru>
2023-04-10 20:19:21 +0900

    go

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // модальное окно
 const wrapper = document.querySelector('.wrapper');
 const headBtn = document.querySelector('.head-btn');
+const burger = document.querySelector('.burger');
 const modalStart = () => {
   const overlay = document.createElement('div');
   const modWindow = document.createElement('div');
@@ -22,8 +23,46 @@ const modalStart = () => {
     overlay.remove();
     document.body.style.overflow = 'visible';
   };
+  if (burger.classList.value == `header__burger burger _burger-active`) {
+    burger.classList.toggle('_burger-active');
+  }
   overlay.addEventListener('click', modalClose);
   closeBtn.addEventListener('click', modalClose);
 };
 headBtn.addEventListener('click', modalStart);
 // модальное окно
+// бургер
+const headerHead = document.querySelector('.header__head');
+for (let i = 1; i <= 3; i++) {
+  const burgerLine = document.createElement('div');
+  burgerLine.classList.add(`burger__line${i}`);
+  burger.append(burgerLine);
+}
+const burgerTrans = () => {
+  burger.classList.toggle('_burger-active');
+  const burgerClose = () => {
+    burger.classList.toggle('_burger-active');
+    headerHead.lastElementChild.previousElementSibling.remove();
+    headerHead.lastElementChild.remove();
+  };
+  if (headerHead.children.length < 4) {
+    const sidebar = document.createElement('div');
+    const overlay = document.createElement('div');
+    const menu = document.querySelector('.menu').cloneNode(true);
+    const socials = document.querySelector('.header__socials').cloneNode(true);
+    socials.className = '_burger-soc';
+    menu.style.display = 'flex';
+    sidebar.classList.add('header__sidebar');
+    overlay.classList.add('overlay');
+    headerHead.append(overlay);
+    sidebar.prepend(menu);
+    sidebar.append(socials);
+    headerHead.append(sidebar);
+    overlay.addEventListener('click', burgerClose);
+  } else {
+    headerHead.lastElementChild.previousElementSibling.remove();
+    headerHead.lastElementChild.remove();
+  }
+};
+burger.addEventListener('click', burgerTrans);
+// бургер
